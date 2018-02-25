5dbc9caea26bac4e809d02a8fef9c675b5479b12
Your Name <you@example.com>
2018-02-25 16:06:53 +0200

    AS

--- app.js
+++ app.js
@@ -18,11 +18,32 @@ var MongoClient = require('mongodb').MongoClient;
 var expstate = require('express-state');
 var compression = require('compression');
 
+
 // require the routes
 var index = require('./routes/index');
 var api = require('./routes/api');
 
 var app = express();
+
+app.use(function (req, res, next) {
+
+	// Website you wish to allow to connect
+	res.setHeader('Access-Control-Allow-Origin', '*');
+
+	// Request methods you wish to allow
+	res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+	// Request headers you wish to allow
+	res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+	// Set to true if you need the website to include cookies in the requests sent
+	// to the API (e.g. in case you use sessions)
+	res.setHeader('Access-Control-Allow-Credentials', true);
+
+	// Pass to next layer of middleware
+	next();
+});
+
 
 // setup the translation
 var i18n = new (require('i18n-2'))({
